c4d7bb7503a968151bdfed841dbc437ca94a6b6b
LiszUehara <ueharalisandra@gmail.com>
2024-07-09 19:28:42 -0300

    :sparkles: feat: Insertion of render with route provider in sidebar test

--- src/components/Sidebar/Sidebar.test.tsx
+++ src/components/Sidebar/Sidebar.test.tsx
@@ -1,20 +1,15 @@
-import { render, screen } from "@testing-library/react"
+import { screen } from "@testing-library/react"
 import user from "@testing-library/user-event";
 import Sidebar from "./index"
+import { customRender } from "../../utils/customRender";
 
 describe('<Sidebar />', () => {
   user.setup();
   it('should the nav items ', async () => {
-    render(<Sidebar/>)
+    customRender(<Sidebar/>)
     const startItem = screen.getByRole('link', { name: /Inicio/i });
     const includeScheduleItem = screen.getByRole('link', { name: /Cadastrar Agendamento/i });
     expect(startItem).toBeInTheDocument();
     expect(includeScheduleItem);
-  })
-  it('change background color', async () => {
-    render(<Sidebar/>)
-    const buttonChangeColor = screen.getByLabelText('change color mode');
-    await user.click(buttonChangeColor);
-    expect(buttonChangeColor);
   })
 })

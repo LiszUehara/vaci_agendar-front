576bccda7a76f381559592b6de55fdc759e737f0
LiszUehara <ueharalisandra@gmail.com>
2024-07-12 18:33:14 -0300

    :sparkles: feat: Creating components for select and textarea

---
+++ src/components/Input/Select.tsx
@@ -0,0 +1,47 @@
+import {
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Select as SelectChacka,
+    SelectProps as SelectChackraProps
+  } from "@chakra-ui/react";
+import { FieldErrors, FieldValues, UseFormRegisterReturn } from "react-hook-form";
+  
+export interface SelectProps extends SelectChackraProps{
+    id: string;
+    label?: string;
+    errors?: FieldErrors<FieldValues>;
+    register?: UseFormRegisterReturn
+    disabled?: boolean;
+    items: {value: string, label: string }[]
+}
+
+  export const Select = ({
+    label,
+    id,
+    errors,
+    isRequired,
+    register,
+    disabled,
+    items,
+    ...props
+  }: SelectProps) => {
+    const isInvalid = errors && !!errors[id];
+    return (
+    <FormControl id={id} isRequired={isRequired} isInvalid={isInvalid}>
+      <FormLabel>{label}</FormLabel>
+      <SelectChacka
+        {...props}
+        disabled={disabled}
+        {...register}
+        _placeholder={{ color: "gray.500" }}
+        _focusVisible={{
+          borderColor: "blue.500",
+          boxShadow: "0 0 0 1px var(--chakra-colors-blue-500)",
+        }}
+        >
+        {items.map(({value, label})=><option value={value}>{label}</option>)}
+      </SelectChacka>
+      {errors && <FormErrorMessage>{typeof errors[id]?.message  === 'string' ? errors[id]?.message : ''}</FormErrorMessage>}
+    </FormControl>
+  )};

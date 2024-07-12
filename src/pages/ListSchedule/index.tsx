edc56c33a7400db8b87d395365005d1d903a0d5d
LiszUehara <ueharalisandra@gmail.com>
2024-07-12 18:31:10 -0300

    :sparkles: feat: Listing screen connection with backend

--- src/pages/ListSchedule/index.tsx
+++ src/pages/ListSchedule/index.tsx
@@ -1,82 +1,124 @@
 import {
-    Box,
     Heading,
     Text,
     Stack,
     Flex,
     IconButton,
-    InputGroup,
-    InputRightElement,
     ListItem,
-    UnorderedList,
     ListIcon,
     List,
     useColorModeValue,
+    useDisclosure,
   } from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
 import { FiCheck, FiClock, FiEdit, FiFileText, FiGift, FiUser } from "react-icons/fi";
-import { Link } from "react-router-dom";
-  const formatter = new Intl.DateTimeFormat("pt-br", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
-  export const ListSchedule = () => {
-    return (
-      <Flex py={6} px={4} rowGap={8} justifyContent={"flex-start"} flexWrap={'wrap'} display={'flex'} columnGap={8}>
-        <Stack width={320} padding={'32px'} borderRadius={'4px'} bgColor={useColorModeValue('white', 'gray.800')} 
-        sx={{boxShadow: '0 0 5px 1px var(--chakra-colors-blue-500)'}}
-        _hover={{boxShadow: '0 0 30px 1px var(--chakra-colors-blue-300)'}}>
-          <Heading
-            color={"gray.400"}
-            fontSize={"2xl"}
-            fontFamily={"body"}
-            display={"flex"}
-            borderBottom={'1px solid'}
-            paddingBottom={2}
-            borderColor={'white'}
-            justifyContent={"space-between"}
-          >
+import fetcher from "../../services/api";
+import { Schedule } from "../../types/list";
+import { ModalEdit } from "../../components/ModalEdit";
+import { AlertContext } from "../../components/contexts/alerts";
+
+const formatterDate = new Intl.DateTimeFormat("pt-br", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+});
+const formatterDateTime = new Intl.DateTimeFormat("pt-br", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+  hour: '2-digit',
+  minute: '2-digit'
+});
+
+function getStatus(status: string | undefined){
+  switch(status){
+    case "completed": return "Concluido";
+    case "cancelled": return "Cancelado";
+    case "unrealized": return "Não Realizado";
+    default: return "Outro";
+  }
+}
+export const ListSchedule = () => {
+  const { error } = useContext(AlertContext);
+  const modalEditDisclosure = useDisclosure()
+  const [list, setList] = useState<Schedule[]>([]);
+  const [item, setItem] = useState<Schedule>({dateTime: new Date(), id: '', patient: { birthDate: new Date(), cpf: '', name: '', id: ''}});
+  useEffect(()=>{
+    setList(JSON.parse(localStorage.getItem('list') ?? ''))
+    fetcher('/api/schedule')
+      .then((res)=> {
+        setList(res.items)
+        localStorage.setItem('list', JSON.stringify(res.items));
+      })
+      .catch(res =>{
+        error(res.cause)})
+  },[])
+  return (
+    <Flex py={6} px={4} rowGap={8} justifyContent={"flex-start"} flexWrap={'wrap'} display={'flex'} columnGap={8}>
+      {list.map(({id, patient, dateTime, status})=>(
+        <Stack
+          key={id} 
+          width={320}
+          padding={'32px'}
+          borderRadius={'4px'}
+          bgColor={useColorModeValue('white', 'gray.800')} 
+          sx={{boxShadow: '0 0 5px 1px var(--chakra-colors-blue-500)'}}
+          _hover={{boxShadow: '0 0 30px 1px var(--chakra-colors-blue-300)'}}>
+            <Heading
+              color={"gray.400"}
+              fontSize={"2xl"}
+              fontFamily={"body"}
+              display={"flex"}
+              borderBottom={'1px solid'}
+              paddingBottom={2}
+              borderColor={'white'}
+              justifyContent={"space-between"}
+            >
               Agendamento
               <IconButton
-                as={Link}
+                aria-label="botão de editar"
                 variant="link"
                 colorScheme="blue.900"
                 size="xl"
                 padding={1}
                 icon={<FiEdit />}
                 _hover={{ color: "blue.900" }}
-                to={`discipline/edit?id=`}
+                onClick={()=> {
+                  modalEditDisclosure.onOpen()
+                  setItem({id, patient, dateTime, status})
+                }}
               />
             </Heading>
-        <List spacing={3}  >
-          <ListItem>
-            <ListIcon as={FiUser} color='blue.600' fontSize={20} />
-            <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>Nome</Text>
-            <Text>Ianca Lisandra</Text>
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FiFileText} color='blue.600' fontSize={20}/>
-            <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>CPF</Text>
-            <Text>123.123.123-00</Text>
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FiGift} color='blue.600' fontSize={20}/>
-            <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>Data de Nascimento</Text>
-            <Text>04/02/2007</Text>
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FiClock} color='blue.600' fontSize={20}/>
-            <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>Data de Agendamento</Text>
-            <Text>19/07/2024 - 10:00</Text>
-          </ListItem>
-          <ListItem>
-            <ListIcon as={FiCheck} color='blue.600' fontSize={20}/>
-            <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>Status</Text>
-            <Text>Aberto</Text>
-          </ListItem>
-        </List>
+            <List spacing={3} >
+              <ListItem>
+                <ListIcon as={FiUser} color='blue.600' fontSize={20} />
+                <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>Nome</Text>
+                <Text>{patient.name}</Text>
+              </ListItem>
+              <ListItem>
+                <ListIcon as={FiFileText} color='blue.600' fontSize={20}/>
+                <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>CPF</Text>
+                <Text>{patient.cpf}</Text>
+              </ListItem>
+              <ListItem>
+                <ListIcon as={FiGift} color='blue.600' fontSize={20}/>
+                <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>Data de Nascimento</Text>
+                <Text>{formatterDate.format(new Date(patient.birthDate))}</Text>
+              </ListItem>
+              <ListItem>
+                <ListIcon as={FiClock} color='blue.600' fontSize={20}/>
+                <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>Data de Agendamento</Text>
+                <Text>{formatterDateTime.format(new Date(dateTime))}</Text>
+              </ListItem>
+              <ListItem>
+                <ListIcon as={FiCheck} color='blue.600' fontSize={20}/>
+                <Text marginBottom={'2px'} color='blue.600' fontSize={20} fontWeight={700}>Status</Text>
+                <Text>{getStatus(status)}</Text>
+              </ListItem>
+            </List>
         </Stack>
-
-      </Flex>
-    )
+      ))}
+      <ModalEdit {...modalEditDisclosure} item={item} />
+    </Flex>
+  )
 }

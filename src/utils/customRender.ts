7475677529f2e4b67a5906b53b611712e57063f6
LiszUehara <ueharalisandra@gmail.com>
2024-07-16 18:57:26 -0300

    :recycle: refactor: test render wrapper update

--- src/utils/customRender.ts
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import { ReactNode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
-
-export const customRender = (ui: ReactNode) => render(ui, { wrapper: BrowserRouter });
-
-export * from '@testing-library/react';
